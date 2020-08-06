2083e2932dd661267dd51665a5f75f28adc37954
rosariov21 <Rosariovelazquez47@yahoo.com>
2020-08-06 09:50:00 -0400

    more pics

--- src/Components/Channels.js
+++ src/Components/Channels.js
@@ -4,11 +4,9 @@ class Channels extends Component{
     render(){
         return(
             <div>
-        
-            <img class="logo" src="http://nautilus-vertical.s3.amazonaws.com/vert/aging/logo_short.svg"/>
+        <img class="chan_img" src="https://s3.amazonaws.com/nautilus-vertical/cosmos_0x0_476x316_a-161.jpeg"/>
             <img class="chan_img" src="https://s3.amazonaws.com/nautilus-vertical/aging_0x0_476x316_a-173.png"/>
-            <h2>Yes, Life in the Fast Lane Kills You</h2>
-            <h4>by Philip Ball</h4>
+           
 
     
             </div>
