376b8db1e3c711ed4975b79baab9285ffa6976c7
rosariov21 <Rosariovelazquez47@yahoo.com>
2020-08-06 09:38:15 -0400

    more

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Home from './Components/Home';
 import Forms from './Components/Forms';
 import Articles from './Components/Articles';
+import Channels from './Components/Channels'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
        <Home/>
     <Articles/>
      <Forms/>
+     <Channels/>
     </div>
   );
 }
