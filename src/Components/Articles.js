3222176a958605f49901141ad189c5adb7f7357e
rosariov21 <Rosariovelazquez47@yahoo.com>
2020-08-06 09:27:17 -0400

    pics

--- src/Components/Articles.js
+++ src/Components/Articles.js
@@ -4,7 +4,10 @@ class Articles extends Component{
     render(){
         return(
           <div>
-            <img src="http://static.nautil.us/16657_881e945c3b5ccf17103ecca2b675585f.png" alt="Altamirano_THUMB" width="314" height="177"/>
+              <a href="/issue/79/catalysts/best-screenplay-goes-to-the-algorithms"/>
+                                                                <img src="http://static.nautil.us/16626_6ac8a357a461cc06579759bbdf12a39b.png" alt="Miller_THUMB" width="314" height="177"/>
+
+            
              <img src="http://static.nautil.us/5307_764a9f2462bf088af07b6ae6c107e62c.png" alt="Supko_THUMB-1" width="314" height="177"/>
                                 
 <img src="http://static.nautil.us/16616_129faba692a0380b8c8fdc5fc01be8c1.png" alt="Berger_THUMB" width="314" height="177"/>
