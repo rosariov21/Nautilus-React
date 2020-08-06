878f018a3c1616e798499dcdb0d8beb19eee37a9
rosariov21 <Rosariovelazquez47@yahoo.com>
2020-08-06 09:45:35 -0400

    more things

--- src/Components/Channels.js
+++ src/Components/Channels.js
@@ -4,7 +4,13 @@ class Channels extends Component{
     render(){
         return(
             <div>
-                
+        
+            <img class="logo" src="http://nautilus-vertical.s3.amazonaws.com/vert/aging/logo_short.svg"/>
+            <img class="chan_img" src="https://s3.amazonaws.com/nautilus-vertical/aging_0x0_476x316_a-173.png"/>
+            <h2>Yes, Life in the Fast Lane Kills You</h2>
+            <h4>by Philip Ball</h4>
+
+    
             </div>
 
         )
