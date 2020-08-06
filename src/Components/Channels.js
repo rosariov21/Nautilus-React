531b4fd394498cd89a715bbaedea3503f2c7c41d
rosariov21 <Rosariovelazquez47@yahoo.com>
2020-08-06 09:55:02 -0400

    more pics

--- src/Components/Channels.js
+++ src/Components/Channels.js
@@ -6,6 +6,8 @@ class Channels extends Component{
             <div>
         <img class="chan_img" src="https://s3.amazonaws.com/nautilus-vertical/cosmos_0x0_476x316_a-161.jpeg"/>
             <img class="chan_img" src="https://s3.amazonaws.com/nautilus-vertical/aging_0x0_476x316_a-173.png"/>
+            <img class="chan_img" src="https://s3.amazonaws.com/nautilus-vertical/tlas_0x0_476x316_a-258.jpeg"/>
+            <img class="chan_img" src="https://s3.amazonaws.com/nautilus-vertical/alliance_0x0_476x316_a-278.png"/>
            
 
     
